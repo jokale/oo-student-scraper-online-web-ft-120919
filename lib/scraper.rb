f83ff6171f4124a9fdcc239b0aa43828f4ef2d48
Joanna E K <kalemajoanna@gmail.com>
2020-01-12 16:59:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,20 @@
+require 'rubygems'
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
-  end
+    index_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
+    html = open(index_url)
+    doc= Nokogiri::HTML(html)
+   end
 
   def self.scrape_profile_page(profile_url)
-    
+         
+     doc = Nokogiri::HTML(open(profile_url))
+     doc.css(". ") 
   end
 
 end
